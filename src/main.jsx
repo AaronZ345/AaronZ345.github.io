97525c13e96f3c6807ba6a0c89eccd89587d3ff8
AaronZ345 <AaronZ345@users.noreply.github.com>
2026-06-02 00:25:11 +0800

    Preload homepage icon fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fortawesome/fontawesome-free/css/solid.min.css";
-import "@fortawesome/fontawesome-free/css/brands.min.css";
-import "academicons/css/academicons.min.css";
 import App from "./App.jsx";
 import "./styles.css";
 
